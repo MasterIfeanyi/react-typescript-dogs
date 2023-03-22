01c9ee94f65321d9393a4655544516d35f181f59
IFEANYI <chimaifeanyi29@gmail.com>
2023-03-23 00:26:31 +0100

    fix api call

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,7 @@ import Error from "./components/Error";
 
 
 function App() {
-
-  // debounced query
-  const {debouncedQuery} = useContext(DataContext)
-  
+ 
 
   // fill the select tag with name of all the breeds
   // const [nameOfBreeds, setNameOfBreeds] = useState<string[]>([])
@@ -22,7 +19,7 @@ function App() {
   // const [breedName, setBreedName] = useState<string>("")
 
   // useContext
-  const {fetchError, setFetchError, imagesOfABreed, setImagesOfABreed} = useContext(DataContext)
+  const {fetchError, debouncedQuery, setFetchError, imagesOfABreed, setImagesOfABreed} = useContext(DataContext)
 
   // when component mounts fill the select tag with names of all the breed
   // useEffect(() => {
@@ -48,10 +45,6 @@ function App() {
   const loadByBreed = async (value: string) => {
     
     
-    // if (!nameOfBreeds.includes(value)) {
-    //   setFetchError("Breed not found")
-    // };
-    
     if(!value && value === "") {
       setFetchError("Please type in a breed name");
       return;
@@ -61,14 +54,10 @@ function App() {
     try {
 
       const response = await fetch(`https://dog.ceo/api/breed/${value}/images`)
-      const data = await response.json()   
-    
+      const data = await response.json();   
 
       if (data.message === "Breed not found (master breed does not exist)"  ) {
-        
-        // setFetchError(data.message.slice(0, 16));
         setFetchError("Breed not found")
-
       }
 
       console.log(data.message)
@@ -77,7 +66,8 @@ function App() {
       
     } catch (error) {
       const err = error as Error
-      console.log(err.message)
+      console.log(err.message);
+      setImagesOfABreed([]);
     }
   }
 
