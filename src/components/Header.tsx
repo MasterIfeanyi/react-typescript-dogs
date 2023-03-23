31149231455b960ce515970c237f65f82150681c
IFEANYI <chimaifeanyi29@gmail.com>
2023-03-23 13:46:36 +0100

    update h1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
       <div className="container">
         <div className="row">
             <div className="col-12 text-center intro-text">
-              <h1 className="mb-3">Dog Finder API</h1>
+              <h1 className="mb-3">Dog Breed Finder</h1>
               <p>This project was created using react-typescript</p>
             </div>
         </div>
